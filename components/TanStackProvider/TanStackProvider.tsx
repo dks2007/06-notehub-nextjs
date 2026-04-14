c76f5b66fb96624250fc6def5f5daf604aa7cca4
Dima Karpinskyi <dimakjkarpinski@gmail.com>
2026-04-14 15:37:12 +0200

    home work

--- components/TanStackProvider/TanStackProvider.tsx
+++ components/TanStackProvider/TanStackProvider.tsx
@@ -2,14 +2,15 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactNode } from 'react';
+import {useState} from "react";
 
-const queryClient = new QueryClient();
 
 interface TanStackProviderProps {
   children: ReactNode;
 }
 
 export default function TanStackProvider({ children }: TanStackProviderProps) {
+  const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
       {children}
